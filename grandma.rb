9de85dec0ddc64ffb8e64d0a6bf26180dedc50d3
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-06-04 18:26:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@
   if phrase == "i love you grandma!".upcase
       "i love you too pumpkin!".upcase
   elsif phrase != phrase.upcase
-    return "no, not since 1938".upcase
+    return "huh? ! speak up, sonny!".upcase"no, not since 1938".upcase
   else
     return 
   end
