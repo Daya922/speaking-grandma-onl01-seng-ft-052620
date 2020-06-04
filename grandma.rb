1edc385765747a8f37e53ba4aa930f23fad382a1
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-06-04 18:08:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
  
  def speak_to_grandma (phrase)
   if phrase == "i love you grandma!".upcase
-    return "i love you too pumpkin!".upcase
+    then "i love you too pumpkin!".upcase
   elsif phrase = "#{phrase}".upcase
     puts "no, not since 1938".upcase
   else
